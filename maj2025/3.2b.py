411d2d6170ac07a340bf90d78d51b38be64a37d7
bohdankiparenko <bohdan.kiparenko@gmail.com>
2026-04-08 09:24:18 +0200

    3.2b was changed for proper work

--- maj2025/3.2b.py
+++ maj2025/3.2b.py
@@ -1,18 +1,24 @@
 folder = "dane/"
-filename = "dron_przyklad.txt"
+filename = "dron.txt"
 
 
 with open(folder + filename) as f:
 	lines = [tuple([int(l) for l in line.strip().split(" ")]) for line in f.readlines()]
 
-i = 0
 point = (0, 0)
 points = []
-while i < len(lines):
-	point = point[0] + lines[i][0], point[1] + lines[i][1]
+for disloc in lines:
+	point = point[0] + disloc[0], point[1] + disloc[1]
 	points.append(point)
-	i += 1
 
-print(points)
+i = 0
 
+
+kopia_points = points[:] # Tworzy kopię listy krotek points
+while i < len(points) - 1:
+	first = points.pop(i)
+	for third in points:
+		second = (first[0] + third[0]) / 2, (first[1] + third [1]) / 2
+		if second in kopia_points:
+			print(first, second, third)
 
